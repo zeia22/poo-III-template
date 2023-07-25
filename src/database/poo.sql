bcc87ca31aac3504d0df966e12a51d1b3a42b8ac
Yuzo <yuzokamoto@gmail.com>
2023-07-25 17:29:40 -0300

    update

--- src/database/poo.sql
+++ src/database/poo.sql
@@ -4,23 +4,25 @@ CREATE TABLE users (
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    created_at TEXT DEFAULT (DATETIME()) NOT NULL
+    created_at TEXT NOT NULL
 );
 
 CREATE TABLE accounts (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     owner_id TEXT NOT NULL,
     balance REAL DEFAULT (0) NOT NULL,
-    created_at TEXT DEFAULT (DATETIME()) NOT NULL,
+    created_at TEXT NOT NULL,
     FOREIGN KEY (owner_id) REFERENCES users (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
 INSERT INTO users (id, name, email, password)
 VALUES
-	("u001", "Fulano", "fulano@email.com", "fulano123"),
-	("u002", "Beltrana", "beltrana@email.com", "beltrana00");
+	('u001', 'Fulano', 'fulano@email.com', 'fulano123'),
+	('u002', 'Beltrana', 'beltrana@email.com', 'beltrana00');
 
 INSERT INTO accounts (id, owner_id)
 VALUES
-	("a001", "u001"),
-	("a002", "u002");
+	('a001', 'u001'),
+	('a002', 'u002');
